dbdcc8b098fe2e49012c23617716edde9f061f73
Kate Opager <opagerk@byu.edu>
2023-02-13 23:08:59 -0700

    Add project files.

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieReview()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieReview(ApplicationResponse ar)
+        {
+            return View("Confirmation", ar);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6/Models/ApplicationResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class ApplicationResponse
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public ushort Year { get; set; }
+        public string Director { get; set; }
+        public byte Rating { get; set; }
+        public bool Edited { get; set; }
+        public int LentTo { get; set; }
+        public string Notes { get; set; }
+    }
+}
