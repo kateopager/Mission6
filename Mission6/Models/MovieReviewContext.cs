dfa4bd13b2eec79cf33b94ac88cf20e5c123a5bc
Kate Opager <opagerk@byu.edu>
2023-02-17 10:16:52 -0700

    .

---
+++ Mission6/Models/MovieReviewContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class MovieReviewContext : DbContext
+    {
+        //this is the constructor that has been created to run when built
+        public MovieReviewContext (DbContextOptions<MovieReviewContext> options) : base (options)
+        {
+
+        }
+        public DbSet<ApplicationResponse> responses { get; set; }
+
+    }
+}
