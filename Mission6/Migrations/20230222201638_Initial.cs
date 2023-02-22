c091251d11ca574d234b0e9404d76f6e93b0660f
Kate Opager <opagerk@byu.edu>
2023-02-22 13:19:18 -0700

    .

---
+++ Mission6/Migrations/20230222201638_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission7.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieReviewID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<ushort>(type: "INTEGER", nullable: false),
+                    Director = table.Column<string>(type: "TEXT", nullable: false),
+                    Rating = table.Column<string>(type: "TEXT", nullable: false),
+                    Edited = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LentTo = table.Column<string>(type: "TEXT", nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true),
+                    CategoryID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieReviewID);
+                    table.ForeignKey(
+                        name: "FK_Responses_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 1, "Musical" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 2, "Action" });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieReviewID", "CategoryID", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, 1, "Kenny Ortega", false, "NA", "NA", "G", "High School Musical", (ushort)2006 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieReviewID", "CategoryID", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, 1, "Kenny Ortega", true, "NA", "NA", "G", "High School Musical 2", (ushort)2007 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieReviewID", "CategoryID", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, 1, "Kenny Ortega", false, "NA", "NA", "G", "High School Musical 3: Senior Year", (ushort)2008 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Responses_CategoryID",
+                table: "Responses",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
